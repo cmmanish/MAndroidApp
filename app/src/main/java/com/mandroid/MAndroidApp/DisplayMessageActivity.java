6bd6820c6d607311d55cdcf97362bd54a295ec73
Manish Madhusoodan <manish.mmadhusoodan@seagate.com>
2015-10-17 09:40:35 -0700

    hello works

--- app/src/main/java/com/mandroid/MAndroidApp/DisplayMessageActivity.java
+++ app/src/main/java/com/mandroid/MAndroidApp/DisplayMessageActivity.java
@@ -1,12 +1,12 @@
 package com.mandroid.MAndroidApp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.widget.TextView;
 
-public class DisplayMessageActivity extends ActionBarActivity {
+public class DisplayMessageActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
